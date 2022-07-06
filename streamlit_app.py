bbdc876055c873de83868142ac5fad1259969969
audgil-snflk <108540634+audgil-snflk@users.noreply.github.com>
2022-07-05 22:02:05 -0600

    Update streamlit_app.py
    Add another function for snowflake input

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,6 @@ try:
 except URLError as e:
   streamlit.error()
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-
 # test snowflake integration
 streamlit.header("The fruit load list contains:")
 
@@ -63,4 +60,14 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.stop()
 
 # interact with snowflake table
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding" + new_fruit
+    
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+
